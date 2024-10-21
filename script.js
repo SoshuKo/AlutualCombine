555a33cbb1bc49dca7d2e6a237110f77f898b8dc
SoshuKo <chitian.kokkota@gmail.com>
2024-10-21 20:51:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -14,160 +14,118 @@ function generateCompoundWord() {
     let generatedWord = '';
     let warningMessage = '';
 
-    // 男性母音の置換関数
+    // 男性母音の置換
     function maleVowelReplacement(word) {
         return word.replace(/e/g, 'a').replace(/i/g, 'ï').replace(/ö/g, 'o').replace(/ü/g, 'u');
     }
 
-    // 女性母音の置換関数
+    // 女性母音の置換
     function femaleVowelReplacement(word) {
         return word.replace(/a/g, 'e').replace(/ï/g, 'i').replace(/o/g, 'ö').replace(/u/g, 'ü');
     }
 
+    // 単語Aの処理
     if (type === 'noun+noun') {
         if (!plural) {
             if (/[e,i,ö,ü]/.test(wordB)) {
                 generatedWord = femaleVowelReplacement(wordA);
-                if (/m$|n$|l$|ng$/.test(wordA)) {
-                    generatedWord += 'e';
-                } else {
-                    generatedWord += 'ne';
-                }
-            }
-            if (/[a,ï,o,u]/.test(wordB)) {
+                generatedWord += /m$|n$|l$|ng$/.test(wordA) ? 'e ' : 'ne ';
+            } else if (/[a,ï,o,u]/.test(wordB)) {
                 generatedWord = maleVowelReplacement(wordA);
-                if (/m$|n$|l$|ng$/.test(wordA)) {
-                    generatedWord += 'a';
-                } else {
-                    generatedWord += 'na';
-                }
+                generatedWord += /m$|n$|l$|ng$/.test(wordA) ? 'a ' : 'na ';
             }
         } else {
             if (/[e,i,ö,ü]/.test(wordB)) {
                 generatedWord = femaleVowelReplacement(wordA);
-                if (/m$|n$|l$|ng$/.test(wordA)) {
-                    generatedWord += 'en';
-                } else {
-                    generatedWord += 'nen';
-                }
-            }
-            if (/[a,ï,o,u]/.test(wordB)) {
+                generatedWord += /m$|n$|l$|ng$/.test(wordA) ? 'en ' : 'nen ';
+            } else if (/[a,ï,o,u]/.test(wordB)) {
                 generatedWord = maleVowelReplacement(wordA);
-                if (/m$|n$|l$|ng$/.test(wordA)) {
-                    generatedWord += 'an';
-                } else {
-                    generatedWord += 'nan';
-                }
+                generatedWord += /m$|n$|l$|ng$/.test(wordA) ? 'an ' : 'nan ';
             }
         }
+        generatedWord += wordB;
     } else if (type === 'adj/auto+noun') {
         if (aspect === 'incomplete') {
             if (/[e,i,ö,ü]/.test(wordB)) {
-                generatedWord = femaleVowelReplacement(wordA);
-                generatedWord = generatedWord.replace(/-n$|-an$|-en$/, 'setön');
-            }
-            if (/[a,ï,o,u]/.test(wordB)) {
-                generatedWord = maleVowelReplacement(wordA);
-                generatedWord = generatedWord.replace(/-n$|-an$|-en$/, 'saton');
+                generatedWord = femaleVowelReplacement(wordA).replace(/-n$|-an$|-en$/, 'setön');
+            } else if (/[a,ï,o,u]/.test(wordB)) {
+                generatedWord = maleVowelReplacement(wordA).replace(/-n$|-an$|-en$/, 'saton');
             }
         } else {
             if (/[e,i,ö,ü]/.test(wordB)) {
-                generatedWord = femaleVowelReplacement(wordA);
-                generatedWord = generatedWord.replace(/-n$|-an$|-en$/, 'seten');
-            }
-            if (/[a,ï,o,u]/.test(wordB)) {
-                generatedWord = maleVowelReplacement(wordA);
-                generatedWord = generatedWord.replace(/-n$|-an$|-en$/, 'satan');
+                generatedWord = femaleVowelReplacement(wordA).replace(/-n$|-an$|-en$/, 'seten');
+            } else if (/[a,ï,o,u]/.test(wordB)) {
+                generatedWord = maleVowelReplacement(wordA).replace(/-n$|-an$|-en$/, 'satan');
             }
         }
+        generatedWord += ' ' + wordB;
 
         if (plural) {
             if (aspect === 'incomplete') {
                 if (/[e,i,ö,ü]/.test(wordB)) {
-                    generatedWord = femaleVowelReplacement(wordA);
-                    generatedWord = generatedWord.replace(/-n$|-an$|-en$/, 'seltön');
-                }
-                if (/[a,ï,o,u]/.test(wordB)) {
-                    generatedWord = maleVowelReplacement(wordA);
-                    generatedWord = generatedWord.replace(/-n$|-an$|-en$/, 'salton');
+                    generatedWord = femaleVowelReplacement(wordA).replace(/-n$|-an$|-en$/, 'seltön');
+                } else if (/[a,ï,o,u]/.test(wordB)) {
+                    generatedWord = maleVowelReplacement(wordA).replace(/-n$|-an$|-en$/, 'salton');
                 }
             } else {
                 if (/[e,i,ö,ü]/.test(wordB)) {
-                    generatedWord = femaleVowelReplacement(wordA);
-                    generatedWord = generatedWord.replace(/-n$|-an$|-en$/, 'selten');
-                }
-                if (/[a,ï,o,u]/.test(wordB)) {
-                    generatedWord = maleVowelReplacement(wordA);
-                    generatedWord = generatedWord.replace(/-n$|-an$|-en$/, 'saltan');
+                    generatedWord = femaleVowelReplacement(wordA).replace(/-n$|-an$|-en$/, 'selten');
+                } else if (/[a,ï,o,u]/.test(wordB)) {
+                    generatedWord = maleVowelReplacement(wordA).replace(/-n$|-an$|-en$/, 'saltan');
                 }
             }
+            generatedWord += ' ' + wordB;
         }
     } else if (type === 'trans+noun') {
         if (aspect === 'incomplete') {
             if (/[e,i,ö,ü]/.test(wordB)) {
-                generatedWord = femaleVowelReplacement(wordA);
-                generatedWord = generatedWord.replace(/-l$|-al$|-el$/, 'sitön');
-            }
-            if (/[a,ï,o,u]/.test(wordB)) {
-                generatedWord = maleVowelReplacement(wordA);
-                generatedWord = generatedWord.replace(/-l$|-al$|-el$/, 'sïton');
+                generatedWord = femaleVowelReplacement(wordA).replace(/-l$|-al$|-el$/, 'sitön');
+            } else if (/[a,ï,o,u]/.test(wordB)) {
+                generatedWord = maleVowelReplacement(wordA).replace(/-l$|-al$|-el$/, 'sïton');
             }
         } else {
             if (/[e,i,ö,ü]/.test(wordB)) {
-                generatedWord = femaleVowelReplacement(wordA);
-                generatedWord = generatedWord.replace(/-l$|-al$|-el$/, 'siten');
-            }
-            if (/[a,ï,o,u]/.test(wordB)) {
-                generatedWord = maleVowelReplacement(wordA);
-                generatedWord = generatedWord.replace(/-l$|-al$|-el$/, 'sïtan');
+                generatedWord = femaleVowelReplacement(wordA).replace(/-l$|-al$|-el$/, 'siten');
+            } else if (/[a,ï,o,u]/.test(wordB)) {
+                generatedWord = maleVowelReplacement(wordA).replace(/-l$|-al$|-el$/, 'sïtan');
             }
         }
+        generatedWord += ' ' + wordB;
 
         if (plural) {
             if (aspect === 'incomplete') {
                 if (/[e,i,ö,ü]/.test(wordB)) {
-                    generatedWord = femaleVowelReplacement(wordA);
-                    generatedWord = generatedWord.replace(/-l$|-al$|-el$/, 'siltön');
-                }
-                if (/[a,ï,o,u]/.test(wordB)) {
-                    generatedWord = maleVowelReplacement(wordA);
-                    generatedWord = generatedWord.replace(/-l$|-al$|-el$/, 'sïlton');
+                    generatedWord = femaleVowelReplacement(wordA).replace(/-l$|-al$|-el$/, 'siltön');
+                } else if (/[a,ï,o,u]/.test(wordB)) {
+                    generatedWord = maleVowelReplacement(wordA).replace(/-l$|-al$|-el$/, 'sïlton');
                 }
             } else {
                 if (/[e,i,ö,ü]/.test(wordB)) {
-                    generatedWord = femaleVowelReplacement(wordA);
-                    generatedWord = generatedWord.replace(/-l$|-al$|-el$/, 'silten');
-                }
-                if (/[a,ï,o,u]/.test(wordB)) {
-                    generatedWord = maleVowelReplacement(wordA);
-                    generatedWord = generatedWord.replace(/-l$|-al$|-el$/, 'sïltan');
+                    generatedWord = femaleVowelReplacement(wordA).replace(/-l$|-al$|-el$/, 'silten');
+                } else if (/[a,ï,o,u]/.test(wordB)) {
+                    generatedWord = maleVowelReplacement(wordA).replace(/-l$|-al$|-el$/, 'sïltan');
                 }
             }
+            generatedWord += ' ' + wordB;
         }
     }
 
     // 複数形語尾の設置
     if (plural) {
         if (/[e,i,ö,ü]/.test(wordB)) {
-            if (/m$|n$|l$|ng$/.test(wordB)) {
-                generatedWord += 'el';
-            } else {
-                generatedWord += 'nel';
-            }
+            generatedWord += /m$|n$|l$|ng$/.test(wordB) ? 'el' : 'nel';
         }
         if (/[a,ï,o,u]/.test(wordB)) {
-            if (/m$|n$|l$|ng$/.test(wordB)) {
-                generatedWord += 'al';
-            } else {
-                generatedWord += 'nal';
-            }
+            generatedWord += /m$|n$|l$|ng$/.test(wordB) ? 'al' : 'nal';
         }
     }
 
+    // 1語にする処理
     if (oneWord) {
         generatedWord = generatedWord.replace(/ /g, '');
     }
 
+    // 文字数制限の警告
     if (generatedWord.length > 15) {
         warningMessage = "「一語にする」をオフにすることを推奨します。";
     }
@@ -184,4 +142,8 @@ function clearInput() {
     document.getElementById('wordB').value = '';
     document.getElementById('result').textContent = '';
     document.getElementById('warning').textContent = '';
+    document.getElementById('type').selectedIndex = 0;
+    document.getElementById('aspect').selectedIndex = 0;
+    document.getElementById('plural').checked = false;
+    document.getElementById('oneWord').checked = false;
 }
